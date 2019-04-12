375f6d154a833b9e4c4742f1b74e52ddbaaa89b6
ria <ria0130@icloud.com>
2019-04-12 17:57:37 +0900

    link icon

--- src/components/organisms/contact.tsx
+++ src/components/organisms/contact.tsx
@@ -9,20 +9,23 @@ export default () => {
     <WhiteSection className="is-medium">
       <Container>
         <SectionTitle tag="h3">Social</SectionTitle>
-        <Columns>
+        <Columns isMobile>
           <ItemColumn isSize={4}>
             <a href="https://twitter.com/_Ria0130" target="_brank">
-              <Icon isSize="large" className="fab fa-twitter fa-4x" /><br/>Twitter
+              <Icon isSize="large" className="fab fa-twitter fa-4x" /><br />
+              Twitter<Icon className="fas fa-external-link-alt" />
             </a>
           </ItemColumn>
           <ItemColumn isSize={4}>
             <a href="https://github.com/ria3100" target="_brank">
-              <Icon isSize="large" className="fab fa-github fa-4x" /><br/>GitHub
+              <Icon isSize="large" className="fab fa-github fa-4x" /><br/>
+              GitHub<Icon className="fas fa-external-link-alt" />
             </a>
           </ItemColumn>
           <ItemColumn isSize={4}>
             <a href="https://www.wantedly.com/secret_profiles/yS5_jktzoGx6wgVJivFK3uefX3OXqz8T" target="_brank">
-              <Icon isSize="large" className="far fa-grin-alt fa-4x" /><br/>Wantedly
+              <Icon isSize="large" className="far fa-grin-alt fa-4x" /><br/>
+              Wantedly<Icon className="fas fa-external-link-alt" />
             </a>
           </ItemColumn>
         </Columns>
