6731aa82b9612c49b258b2b3cefd536167b69f7d
ria <ria0130@icloud.com>
2019-06-04 16:41:31 +0900

    ogp

--- src/components/pages/detail.tsx
+++ src/components/pages/detail.tsx
@@ -7,7 +7,6 @@ import { DetailTemplate } from '../templates'
 export default ({ data }: any) => {
   const { markdownRemark } = data
   const { frontmatter, html } = markdownRemark
-
   return (
     <>
       <Helmet
@@ -15,10 +14,9 @@ export default ({ data }: any) => {
         meta={[
           { name: 'description', content: frontmatter.title },
           { property: 'og:type', content: 'blog' },
-          // { property: 'og:description', content: 'description' },
           { property: 'og:title', content: frontmatter.title },
-          // { property: 'og:url', content: 'url' },
-          // { property: 'og:image', content: 'image' },
+          { property: 'og:url', content: `https://ria-blog.org/${frontmatter.path}` },
+          { property: 'og:image', content: 'https://ria-blog.org/icons/icon-512x512.png' },
           { property: 'og:site_name', content: 'Ria' },
           { property: 'og:locale', content: 'ja_JP' },
         ]}
