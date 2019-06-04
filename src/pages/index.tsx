06178c398a9d816b6d72d73afc36d9f30f160bb9
ria <ria0130@icloud.com>
2019-06-04 16:47:07 +0900

    ogp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,6 +22,12 @@ export default ({ data }) => {
         title="Ria"
         meta={[
           { name: 'description', content: 'Ria' },
+          { property: 'og:type', content: 'blog' },
+          { property: 'og:title', content: 'Ria' },
+          { property: 'og:url', content: `https://ria-blog.org/` },
+          { property: 'og:image', content: 'https://ria-blog.org/icons/icon-512x512.png' },
+          { property: 'og:site_name', content: 'Ria' },
+          { property: 'og:locale', content: 'ja_JP' },
         ]}
       />
       <IndexTemplate listData={listData} />
